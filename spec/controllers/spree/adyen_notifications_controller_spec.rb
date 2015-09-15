5da83bf54be83865044ca2e39d619cb3599aa231
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-15 15:59:09 -0700

    Cleaned up test
    Nothing new, just reorganized.

--- spec/controllers/spree/adyen_notifications_controller_spec.rb
+++ spec/controllers/spree/adyen_notifications_controller_spec.rb
@@ -2,42 +2,50 @@ require 'spec_helper'
 
 module Spree
   describe AdyenNotificationsController do
-    context "request authenticated" do
-      before do
-        ENV["ADYEN_NOTIFY_USER"] = "username"
-        ENV["ADYEN_NOTIFY_PASSWD"] = "password"
-        @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("username:password")
+
+    let(:order) { create :order }
+    let(:params) do
+      { "pspReference" => "8513823667306210",
+        "eventDate" => "2013-10-21T14:45:45.93Z",
+        "merchantAccountCode" => "Test",
+        "reason" => "41061:1111:6/2016",
+        "originalReference" => "",
+        "value" => "6999",
+        "eventCode" => "AUTHORISATION",
+        "merchantReference" => order.id,
+        "operations" => "CANCEL,CAPTURE,REFUND",
+        "success" => "true",
+        "paymentMethod" => "visa",
+        "currency" => "USD",
+        "live" => "false" }
+    end
+
+    before do
+      ENV["ADYEN_NOTIFY_USER"] = "username"
+      ENV["ADYEN_NOTIFY_PASSWD"] = "password"
+    end
+
+    describe "POST notify" do
+      subject { spree_post :notify, params }
+
+      context "request authenticated" do
+        before { bypass_auth }
+
+        it { is_expected.to have_http_status :ok }
+
+        it "creates a notification" do
+          expect{ subject }.to change { AdyenNotification.count }.from(0).to(1)
+        end
       end
 
-      let(:order) { create :order }
-      let(:params) do
-        { "pspReference" => "8513823667306210",
-          "eventDate" => "2013-10-21T14:45:45.93Z",
-          "merchantAccountCode" => "Test",
-          "reason" => "41061:1111:6/2016",
-          "originalReference" => "",
-          "value" => "6999",
-          "eventCode" => "AUTHORISATION",
-          "merchantReference" => order.id,
-          "operations" => "CANCEL,CAPTURE,REFUND",
-          "success" => "true",
-          "paymentMethod" => "visa",
-          "currency" => "USD",
-          "live" => "false" }
-      end
-
-      it "logs notitification" do
-        expect {
-          spree_post :notify, params
-        }.to change { AdyenNotification.count }.by(1)
+      context "request not authenticated" do
+        it { is_expected.to have_http_status 401 }
       end
     end
 
-    context "request not authenticated" do
-      it "logs notitification" do
-        spree_post :notify
-        expect(response.status).to eq 401
-      end
+    def bypass_auth
+      @request.env["HTTP_AUTHORIZATION"] = "Basic " +
+        Base64::encode64("username:password")
     end
   end
 end
