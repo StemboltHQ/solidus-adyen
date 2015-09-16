44ed7df193198b7d85a40bf500867befd0cfd527
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-16 13:49:24 -0700

    Remove conditional block
    As far as i can see, this doesn't work and never did. There's no tests
    for this control block, and in development it silently fails - the first
    time the notification is logged but when
    @notification.original_reference is called it triggers an undefined
    method exception. The exception causes the controller to never response
    to adyen, which then sends the notification again - which is caught in
    the duplicate record catch, allowing it to ack adyen.

--- app/controllers/spree/adyen_notifications_controller.rb
+++ app/controllers/spree/adyen_notifications_controller.rb
@@ -7,15 +7,6 @@ module Spree
     def notify
       notification = AdyenNotification.log(params)
       notification.handle!
-
-      if notification.event_code == "REFUND" && notification.success?
-        #this exists purely to log the transaction ID of the refund. It might make sense to
-        #refactor this so that it lives elswhere.
-        Spree::Payment.find_by(response_code: @notification.original_reference)
-        refund = Spree::Refund.where(payment_id: @payment.id).last
-        refund.transaction_id = @notification.psp_reference
-        refund.save
-      end
     rescue ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid
       # Validation failed, because of the duplicate check.
       # So ignore this notification, it is already stored and handled.
