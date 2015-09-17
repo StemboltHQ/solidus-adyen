509962d2f16e75d4f3b1ba50b17ae28f1030072e
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-17 13:36:25 -0700

    require pry in test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ require 'rspec/active_model/mocks'
 require 'vcr'
 require 'ffaker'
 require 'shoulda/matchers'
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
