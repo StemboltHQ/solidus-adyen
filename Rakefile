5ceca2f4a616ae64f51d188d7ceba0a9dc6c9caf
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-18 09:54:51 -0700

    Fixed references to spree-adyen
    The partial rename was preventing the test app from being generated. Can
    actually run tests now!

--- Rakefile
+++ Rakefile
@@ -4,6 +4,6 @@ task :default => [:spec]
 
 desc 'Generates a dummy app for testing'
 task :test_app do
-  ENV['LIB_NAME'] = 'spree-adyen'
+  ENV['LIB_NAME'] = 'solidus-adyen'
   Rake::Task['common:test_app'].invoke
 end
--- lib/spree/adyen/engine.rb
+++ lib/spree/adyen/engine.rb
@@ -1,17 +1,17 @@
 module Spree
   module Adyen
     class Engine < ::Rails::Engine
-      engine_name "spree-adyen"
+      engine_name "solidus-adyen"
 
       isolate_namespace Spree::Adyen
 
-      initializer "spree.spree-adyen.payment_methods", :after => "spree.register.payment_methods" do |app|
+      initializer "spree.solidus-adyen.payment_methods", :after => "spree.register.payment_methods" do |app|
         app.config.spree.payment_methods << Gateway::AdyenPayment
         app.config.spree.payment_methods << Gateway::AdyenHPP
         app.config.spree.payment_methods << Gateway::AdyenPaymentEncrypted
       end
 
-      initializer "spree-adyen.assets.precompile", :group => :all do |app|
+      initializer "solidus-adyen.assets.precompile", :group => :all do |app|
         app.config.assets.precompile += %w[
           adyen.encrypt.js
         ]
