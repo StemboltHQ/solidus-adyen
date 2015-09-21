df6877c32f8f3c37ebc5810353b2ed255186281d
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-09-21 09:18:09 -0700

    Cleaned up adyen url building
    Methods in spree adyen form build uri's now and actually go through the
    adyen gem's param building functions.

--- lib/spree/adyen/form.rb
+++ lib/spree/adyen/form.rb
@@ -2,26 +2,68 @@ require "json"
 
 module Spree::Adyen::Form
   Form = Adyen::Form
+  UrlHelper = Object.new.extend ActionView::Helpers::UrlHelper
 
   class << self
-    def payment_methods_from_directory params
-      payment_methods(params.dup).map do |method|
-        method.merge("url" => url('details', params.merge(brand_code: method['brandCode'])).to_s)
-      end
+    def payment_methods_from_directory order, payment_method
+      payment_methods(order, payment_method).fetch('paymentMethods')
+    end
+
+
+    def directory_url order, payment_method
+      endpoint_url 'directory', order, payment_method
+    end
+
+    def details_url order, payment_method, directory_entry
+      endpoint_url(
+        'details', order, payment_method, directory_entry.slice('brandCode'))
+    end
+
+    def endpoint_url endpoint, order, payment_method, opts = {}
+      base = URI::parse(url payment_method, endpoint)
+
+      URI::HTTPS.build(
+        host: base.host,
+        path: base.path,
+        query: params(order, payment_method).merge(opts).to_query)
     end
 
     private
-    def url endpoint, params
-      # this is bad and I feel bad
-      URI Form.redirect_url(params).sub('select.shtml', "#{endpoint}.shtml")
+    def payment_methods order, payment_method
+      url = ::Spree::Adyen::Form.directory_url(order, payment_method)
+
+      JSON.parse ::Net::HTTP.get url
     end
 
-    def payment_methods params
-      response(params).fetch('paymentMethods', [])
+    def url payment_method, endpoint
+      server = payment_method.preferences.fetch(:server)
+      Form.url(server, endpoint)
     end
 
-    def response params
-      JSON.parse Net::HTTP.get url('directory', params)
+    def params order, payment_method
+      Form.flat_payment_parameters default_params.
+        merge(order_params order).
+        merge(payment_method_params payment_method)
+    end
+
+    # TODO set this in the adyen config
+    def default_params
+      { currency_code: Spree::Config.currency,
+        ship_before_date: Date.tomorrow,
+        session_validity: 10.minutes.from_now,
+        recurring: false }
+    end
+
+    def order_params order
+      { merchant_reference: order.number.to_s,
+        country_code: order.billing_address.country.iso3,
+        payment_amount: (order.total.to_f * 100).to_int }
+    end
+
+    def payment_method_params payment_method
+      { merchant_account: payment_method.merchant_account,
+        skin_code: payment_method.skin_code,
+        shared_secret: payment_method.shared_secret }
     end
   end
 end
---
+++ spec/lib/spree/adyen/form_spec.rb
@@ -0,0 +1,63 @@
+require "spec_helper"
+
+RSpec.describe Spree::Adyen::Form do
+  describe "directory_url" do
+    let(:order) { create :order, total: 99.0 }
+
+    let(:payment_method) { create :hpp_gateway, preferences: preferences }
+
+    let(:preferences){
+      {server: "test",
+       test_mode: true,
+       api_username: "username",
+       api_password: "password",
+       merchant_account: "account",
+       skin_code: "XXXXXX",
+       shared_secret: "1234567890"}
+    }
+
+    let(:expected) do
+      redirect_params = {
+        currency_code: Spree::Config.currency,
+        ship_before_date: Date.tomorrow,
+        session_validity: 10.minutes.from_now,
+        recurring: false,
+        merchant_reference: order.number.to_s,
+        merchant_account: payment_method.merchant_account,
+        skin_code: payment_method.skin_code,
+        shared_secret: payment_method.shared_secret,
+        country_code: order.billing_address.country.iso3,
+        payment_amount: (order.total.to_f * 100).to_int }
+
+       ::Adyen::Form.redirect_url(redirect_params)
+    end
+
+    subject { described_class.directory_url order, payment_method  }
+
+    it "has the same query options as Adyen gem's" do
+      expect(hash_query subject).to eq hash_query expected
+    end
+
+    it 'has the proper protocol' do
+      expect(URI(subject).scheme).to eq 'https'
+    end
+
+    it 'the right host' do
+      expect(URI(subject).host).to eq 'test.adyen.com'
+    end
+
+    context 'when in production' do
+      before do
+        payment_method.preferences[:server] = 'live'
+      end
+
+      it 'has the proper protocol and host' do
+        expect(URI(subject).host).to eq 'live.adyen.com'
+      end
+    end
+  end
+
+  def hash_query url
+    CGI::parse URI(url).query
+  end
+end
