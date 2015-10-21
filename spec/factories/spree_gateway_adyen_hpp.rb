0bd100123924bb9d5fcbbb6891f4a4a957a31d8c
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-10-21 13:59:20 -0700

    Added a factory to create an env configured gateway

--- spec/factories/spree_gateway_adyen_hpp.rb
+++ spec/factories/spree_gateway_adyen_hpp.rb
@@ -7,6 +7,17 @@ FactoryGirl.define do
       skin_code: 'XXXXX',
       shared_secret: '1234',
       merchant_account: 'XXXX',
-      days_to_ship: 3)
+      days_to_ship: 3
+    )
+
+    trait :env_configured do
+      preferred_test_mode true
+      preferred_days_to_ship 1
+      preferred_api_password { ENV.fetch("ADYEN_API_PASSWORD") }
+      preferred_api_username { ENV.fetch("ADYEN_API_USERNAME") }
+      preferred_merchant_account { ENV.fetch("ADYEN_MERCHANT_ACCOUNT") }
+      preferred_shared_secret { ENV.fetch("ADYEN_SKIN_CODE") }
+      preferred_skin_code { ENV.fetch("ADYEN_SKIN_CODE") }
+    end
   end
 end
