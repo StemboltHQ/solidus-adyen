ef9846ab097a0574beed4f12ccc91ebc9cc4a3d8
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-11-03 14:09:03 -0800

    manual refund links to adyen customer portal
    Clicking on the "manual refund required" button will take the user to
    the customer portal and pull up all the payments for the order.

--- lib/spree/adyen/url.rb
+++ lib/spree/adyen/url.rb
@@ -10,4 +10,16 @@ module Spree::Adyen::URL
       }.to_query
     ).to_s
   end
+
+  def self.modify_search_url(query:)
+    ::URI::HTTPS.build(
+      host: "ca-test.adyen.com",
+      path: "/ca/ca/payments/modifySearch.shtml",
+      query:
+      { uxEvent: "PAYMENT_SEARCH",
+        query: query,
+        search: "Search"
+      }.to_query
+    ).to_s
+  end
 end
