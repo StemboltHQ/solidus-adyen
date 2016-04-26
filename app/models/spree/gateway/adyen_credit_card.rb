7137ae4d5c3e553af020ba902049831efd410321
Martin Meyerhoff <mamhoff@gmail.com>
2016-04-26 11:29:44 +0200

    Add basic gateway methods
    We have a provider class and a way to reference the preferences.

--- app/models/spree/gateway/adyen_credit_card.rb
+++ app/models/spree/gateway/adyen_credit_card.rb
@@ -1,7 +1,35 @@
 module Spree
   class Gateway::AdyenCreditCard < Gateway
+    preference :api_username, :string
+    preference :api_password, :string
+    preference :merchant_account, :string
+    preference :cse_token, :string
+
+    def merchant_account
+      ENV["ADYEN_MERCHANT_ACCOUNT"] || preferred_merchant_account
+    end
+
+    def method_type
+      "adyen_encrypted_cc"
+    end
+
+    def cse_token
+      ENV["ADYEN_CSE_TOKEN"] || preferred_cse_token
+    end
+
     def provider_class
       ::Adyen::API
+    end
+
+    def provider
+      ::Adyen.configuration.api_username =
+        (ENV["ADYEN_API_USERNAME"] || preferred_api_username)
+      ::Adyen.configuration.api_password =
+        (ENV["ADYEN_API_PASSWORD"] || preferred_api_password)
+      ::Adyen.configuration.default_api_params[:merchant_account] =
+        merchant_account
+
+      provider_class
     end
   end
 end
--- spec/models/spree/gateway/adyen_credit_card_spec.rb
+++ spec/models/spree/gateway/adyen_credit_card_spec.rb
@@ -9,10 +9,40 @@ describe Spree::Gateway::AdyenCreditCard do
     it { is_expected.to eq(Adyen::API) }
   end
 
+  describe 'method_type' do
+    subject { described_class.new.method_type }
+
+    it { is_expected.to eq("adyen_encrypted_cc") }
+  end
+
   describe 'payment source class' do
     subject { described_class.new.payment_source_class }
 
     it { is_expected.to eq(Spree::CreditCard) }
   end
 
+  describe 'cse_token' do
+    subject { described_class.new.cse_token }
+
+    context "with no preference set" do
+      before { ENV["ADYEN_CSE_TOKEN"] = nil }
+
+      it { is_expected.to eq(nil) }
+
+      context "with an environment key set" do
+        before { ENV["ADYEN_CSE_TOKEN"] = "SUPERTOKEN" }
+        after { ENV["ADYEN_CSE_TOKEN"] = nil}
+
+        it { is_expected.to eq("SUPERTOKEN") }
+      end
+    end
+
+    context "with a preference set" do
+      subject do
+        described_class.new(preferred_cse_token: "SOMETHING").cse_token
+      end
+
+      it { is_expected.to eq("SOMETHING") }
+    end
+  end
 end
