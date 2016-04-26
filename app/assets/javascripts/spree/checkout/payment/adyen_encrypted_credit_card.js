1710be7df568862c698a753dc32dcfa65f59c56b
Martin Meyerhoff <mamhoff@gmail.com>
2016-04-26 18:59:48 +0200

    Include Adyen JS on the payment form

--- app/assets/javascripts/spree/checkout/payment/adyen_encrypted_credit_card.js
+++ app/assets/javascripts/spree/checkout/payment/adyen_encrypted_credit_card.js
@@ -1,1 +1,16 @@
-
+Spree.createEncryptedAdyenForm = function(paymentMethodId) { 
+  document.addEventListener("DOMContentLoaded", function() {
+    var checkout_form = document.getElementById("checkout_form_payment")
+      // See adyen.encrypt.simple.html for details on the options to use
+    var options = {
+      name: "payment_source[" + paymentMethodId + "][encrypted_credit_card_data]",
+      enableValidations : true,
+      // If there's other payment methods, they need to be able to submit
+      submitButtonAlwaysEnabled: true,
+      disabledValidClass: true
+    };
+    // Create the form.
+    // Note that the method is on the Adyen object, not the adyen.encrypt object.
+    return adyen.createEncryptedForm(checkout_form, options);
+  });
+}
