e67d32faa3d3db5d3862afe3b8a41d399c64fa0d
Martin Meyerhoff <mamhoff@gmail.com>
2016-04-26 11:30:32 +0200

    Add special form with encrypted fields
    The form holds JS include tags for custom JS as well as
    JS include tags for Adyens Easy Encryption library - to be
    fetched from their servers using the CSE token.

--- config/initializers/solidus_adyen.rb
+++ config/initializers/solidus_adyen.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.precompile += %w( spree/checkout/payment/adyen.js )
+Rails.application.config.assets.precompile += %w( spree/checkout/payment/adyen.js spree/checkout/payment/adyen_encrypted_credit_card.js)
 
 Spree::Adyen::HPP.configure do |config|
   config.params_class = Spree::Adyen::HPP::Params
