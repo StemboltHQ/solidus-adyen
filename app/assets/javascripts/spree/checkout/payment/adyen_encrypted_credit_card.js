c3f9b0312ff22ff8868e33eec36fe10b8a483920
Martin Meyerhoff <mamhoff@gmail.com>
2016-04-27 13:33:22 +0200

    Validate Encrypted Card form on submit - Step 1
    This has been surprisingly hard. Spree adds submit handlers to the form,
    and we do as well. They conflict with each other, so I have to unbind and re-bind them.

--- app/assets/javascripts/spree/checkout/payment/adyen_encrypted_credit_card.js
+++ app/assets/javascripts/spree/checkout/payment/adyen_encrypted_credit_card.js
@@ -1,16 +1,37 @@
 Spree.createEncryptedAdyenForm = function(paymentMethodId) { 
-  document.addEventListener("DOMContentLoaded", function() {
-    var checkout_form = document.getElementById("checkout_form_payment")
-      // See adyen.encrypt.simple.html for details on the options to use
-    var options = {
-      name: "payment_source[" + paymentMethodId + "][encrypted_credit_card_data]",
-      enableValidations : true,
-      // If there's other payment methods, they need to be able to submit
-      submitButtonAlwaysEnabled: false,
-      disabledValidClass: true
-    };
-    // Create the form.
-    // Note that the method is on the Adyen object, not the adyen.encrypt object.
-    return adyen.createEncryptedForm(checkout_form, options);
-  });
+  var checkout_form = document.getElementById("checkout_form_payment")
+    // See adyen.encrypt.simple.html for details on the options to use
+  var options = {
+    name: "payment_source[" + paymentMethodId + "][encrypted_credit_card_data]",
+    // We want the validations only to fire when we hit the submit button
+    enableValidations : false,
+    // If there's other payment methods, they need to be able to submit
+    submitButtonAlwaysEnabled: true,
+    disabledValidClass: true
+  };
+
+  // Create the form.
+  // Note that the method is on the Adyen object, not the adyen.encrypt object.
+  return adyen.createEncryptedForm(checkout_form, options);
+};
+
+Spree.attachAdyenFormSubmit = function() {
+  var checkout_form = $("#checkout_form_payment");
+
+  // Unbind all other events from submit button
+  checkout_form.unbind("submit")
+  checkout_form.submit(Spree.handleAdyenFormSubmit)
+}
+
+Spree.detachAdyenFormSubmit = function() {
+  var checkout_form = $("#checkout_form_payment");
+  checkout_form.unbind("submit")
+  checkout_form.submit(Spree.disableSaveOnClick)
+}
+
+Spree.handleAdyenFormSubmit = function(e) {
+  alert("Submit button clicked");
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  return false;
 }
