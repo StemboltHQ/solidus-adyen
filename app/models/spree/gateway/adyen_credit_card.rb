788295122734f6fa4b7ca1703808e34a7353d378
Martin Meyerhoff <mamhoff@gmail.com>
2016-04-29 11:26:30 +0200

    Order configuration methods alphabetically

--- app/models/spree/gateway/adyen_credit_card.rb
+++ app/models/spree/gateway/adyen_credit_card.rb
@@ -1,9 +1,9 @@
 module Spree
   class Gateway::AdyenCreditCard < Gateway
-    preference :api_username, :string
     preference :api_password, :string
-    preference :merchant_account, :string
+    preference :api_username, :string
     preference :cse_token, :string
+    preference :merchant_account, :string
 
     def api_password
       ENV["ADYEN_API_PASSWORD"] || preferred_api_password
@@ -11,6 +11,10 @@ module Spree
 
     def api_username
       ENV["ADYEN_API_USERNAME"] || preferred_api_username
+    end
+
+    def cse_token
+      ENV["ADYEN_CSE_TOKEN"] || preferred_cse_token
     end
 
     def merchant_account
@@ -21,10 +25,6 @@ module Spree
       "adyen_encrypted_cc"
     end
 
-    def cse_token
-      ENV["ADYEN_CSE_TOKEN"] || preferred_cse_token
-    end
-
     def provider_class
       ::Adyen::API
     end
@@ -32,8 +32,7 @@ module Spree
     def provider
       ::Adyen.configuration.api_username = api_username
       ::Adyen.configuration.api_password = api_password
-      ::Adyen.configuration.default_api_params[:merchant_account] =
-        merchant_account
+      ::Adyen.configuration.default_api_params[:merchant_account] = merchant_account
 
       provider_class
     end
