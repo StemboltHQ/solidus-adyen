91e24943bad96f9083188056f940fa24b8dba60a
Martin Meyerhoff <mamhoff@gmail.com>
2016-05-02 11:55:21 +0200

    Make specs run without API credentials
    We want full integration specs to run, but we don't want actual API
    credentials to be present for that.
    
    In order to accomplish this, I needed to download the JS library Adyen offers
    and use the downloaded version in the specs.

--- config/initializers/solidus_adyen.rb
+++ config/initializers/solidus_adyen.rb
@@ -1,1 +1,8 @@
-Rails.application.config.assets.precompile += %w( spree/checkout/payment/adyen.js spree/checkout/payment/adyen_encrypted_credit_card.js)
+Rails.application.config.assets.precompile += %w(
+  spree/checkout/payment/adyen.js
+  spree/checkout/payment/adyen_encrypted_credit_card.js
+)
+
+Rails.application.config.assets.precompile += %w(
+  test-adyen-encrypt.js
+) if Rails.env.test?
