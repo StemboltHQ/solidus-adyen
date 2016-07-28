91529ad573fc9b95e6a1823a2f32705f48fc97ee
Luuk Veenis <luuk@stembolt.com>
2016-07-28 16:50:19 -0700

    Refactor credit card payments
    This includes a number of major changes to how credit cards work in the
    gem:
    - notifications are now associated with credit card payments
    - credit card payments are transitioned based on notifications like HPP
    - support for payment profiles was removed
    - authorisation happens in the payment step
    - auto_capture is properly supported on the cc payment method
    
    The reason to auth in the payment step is that Adyen did not like that
    we were doing $0 auths to "create profiles". Since this is the only step
    we have access to the encrypted card data, we have to perform the auth
    here. The only alternatives are to change the checkout flow or persist
    the encrypted card data (neither of which are desirable).
    
    Payment profile support was removed because Adyen doesn't truly support
    payment profiles. They support the notion of recurring payment
    contracts, which require a first payment transaction to store the
    customer's details. This is why there was initially a $0 authorization
    that passed in the flag to create a recurring contract.
    
    I had to override #purchase! for credit cards to only call #capture!
    instead, since we'll have already authorized the payment at that point.
    
    NOTE: The biggest concern with this implementation is that if a customer
    abandons checkout in the confirm step, there will be an outstanding
    authorization on the card. Anyone using this functionality should
    regularly cancel old authorizations.

--- spec/models/spree/gateway/adyen_credit_card_spec.rb
+++ spec/models/spree/gateway/adyen_credit_card_spec.rb
@@ -98,114 +98,12 @@ describe Spree::Gateway::AdyenCreditCard do
 
   describe 'payment_profiles_supported?' do
     subject { described_class.new.payment_profiles_supported? }
-    it { is_expected.to be true }
-  end
-
-  describe 'create_profile' do
-    let(:order) { build_stubbed(:order)}
-    let(:gateway) { described_class.new }
-    let(:payment) { build_stubbed(:payment, source: card, order: order) }
-
-    subject { gateway.create_profile(payment) }
-
-    context "when the card already has a customer profile" do
-      let(:card) { build_stubbed(:credit_card, gateway_customer_profile_id: "APROFILE") }
-
-      it "does no calls and returns nil" do
-        expect(gateway.provider).not_to receive(:authorize_payment)
-        expect(subject).to be nil
-      end
-    end
-
-    context "when the card has no customer profile yet" do
-      let(:order) do
-        build_stubbed(
-          :order,
-          user_id: 5565,
-          email: "hello@mydomain.com",
-          last_ip_address: "1.2.3.4",
-          number: "MYORDERNUMBER",
-          currency: "RUB"
-        )
-      end
-
-      let(:card) { build_stubbed(:credit_card, number: nil, encrypted_data: "HARDENCRYPTEDDATA") }
-      let(:card_list) { Adyen::API::RecurringService::ListResponse.new(nil) }
-      it "authorizes a payment with zero dollars in the correct currency" do
-        expect(gateway.provider).to receive(:authorise_payment).with(
-          5565,
-          { value: 0, currency: "RUB" },
-          { reference: 5565, email: "hello@mydomain.com", ip: "1.2.3.4", statement: "MYORDERNUMBER"},
-          { encrypted: { json: "HARDENCRYPTEDDATA" } },
-          true
-        ).and_return(OpenStruct.new(success?: true))
-        expect(card_list).to receive(:details).and_return([])
-        expect(gateway.provider).to receive(:list_recurring_details).with(5565).and_return(card_list)
-        subject
-      end
-
-      context "and the card is valid" do
-        let(:card) { create(:credit_card, last_digits: "", number: "", encrypted_data: "OHSOENCRYPTED") }
-
-        let(:registered_card_details) do
-          {
-            recurring_detail_reference: "CARDIDATADYEN",
-            variant: "visa",
-            card: {
-              expiry_date: Date.new(2015, 06, 30),
-              holder_name: "Johnny Doe",
-              number: "1111"
-            }
-          }
-        end
-
-        before do
-          expect(gateway.provider).to receive(:authorise_payment).and_return(OpenStruct.new(success?: true))
-          expect(gateway.provider).to receive(:list_recurring_details).with(5565).and_return(card_list)
-          expect(card_list).to receive(:details).and_return([registered_card_details])
-        end
-
-        it "it populates the credit card object with a customer profile" do
-          expect { subject }.to change { card.gateway_customer_profile_id }.to("CARDIDATADYEN")
-        end
-
-        it "populates the credit card object with a credit card type" do
-          expect { subject }.to change { card.cc_type }.to("visa")
-        end
-
-        it "populates the credit card object with the last digits" do
-          expect { subject }.to change { card.last_digits }.to("1111")
-        end
-
-        it "populates the credit card with a month" do
-          expect { subject }.to change { card.month }.to("06")
-        end
-
-        it "populates the credit card with a year" do
-          expect { subject }.to change { card.year }.to("2015")
-        end
-
-        it "populates the credit card with a name" do
-          expect { subject }.to change { card.name }.to("Johnny Doe")
-        end
-      end
-
-      context "and the card is submitted unencrypted" do
-        let(:card) { build(:credit_card, number: "4111 1111 1111 1111") }
-        let(:payment) { build(:payment, source: card) }
-
-        subject { gateway.create_profile(payment) }
-
-        it "raises an error" do
-          expect { subject }.to raise_error(described_class::ClearTextCardNumberError)
-        end
-      end
-    end
+    it { is_expected.to be false }
   end
 
   describe 'authorize' do
+    subject { gateway.authorize(2000, card, gateway_options) }
     let(:gateway) { described_class.new }
-    let(:adyen_response) { Adyen::API::PaymentService::AuthorisationResponse.new(nil) }
     let(:gateway_options) do
       {
         order_id: "R423936067-5D5ZHURX",
@@ -217,123 +115,11 @@ describe Spree::Gateway::AdyenCreditCard do
     end
     let(:card) { stub_model(Spree::CreditCard, gateway_customer_profile_id: "CARDIDATADYEN") }
 
-    before do
-      expect(adyen_response).to receive(:success?).at_least(:once) { true }
-      expect(adyen_response).to receive(:params).at_least(:once) { adyen_params }
-      expect(adyen_response).to receive(:psp_reference) { adyen_params[:psp_reference] }
-    end
+    it { is_expected.to be_a(ActiveMerchant::Billing::Response) }
+    it { is_expected.to be_success }
 
-    context "when payment is successful" do
-      let(:adyen_params) do
-        {
-          psp_reference: "BEAUTIFULREFERENCE",
-          result_code: "Authorised",
-          auth_code: "ISSUER_AUTH",
-          additional_data: {},
-          refusal_reason: ""
-        }
-      end
-
-      it "calls the Adyen service with the right options and returns the correct object" do
-        expect(gateway.provider).to receive(:authorise_recurring_payment).with(
-          "R423936067",
-          { value: 2000, currency: "USD" },
-          { reference: 1, email: "spree@example.com", ip: "1.2.3.4", statement: "R423936067-5D5ZHURX" },
-          "CARDIDATADYEN",
-          nil,
-          false
-        ).and_return(adyen_response)
-
-        response = gateway.authorize(2000, card, gateway_options)
-        expect(response.success?).to be true
-        expect(response.message).to eq(
-          JSON.pretty_generate({
-            "psp_reference" => "BEAUTIFULREFERENCE",
-            "result_code" => "Authorised",
-            "auth_code" => "ISSUER_AUTH",
-            "additional_data" => {},
-            "refusal_reason" => ""
-          })
-        )
-        expect(response.params).to eq(
-          {
-            "psp_reference" => "BEAUTIFULREFERENCE",
-            "result_code" => "Authorised",
-            "auth_code" => "ISSUER_AUTH",
-            "additional_data" => {},
-            "refusal_reason" => ""
-          }
-        )
-        expect(response.authorization).to eq("BEAUTIFULREFERENCE")
-        expect(response.error_code).to be_nil
-      end
-    end
-  end
-
-  describe 'purchase' do
-    let(:gateway) { described_class.new }
-    let(:adyen_response) { Adyen::API::PaymentService::AuthorisationResponse.new(nil) }
-    let(:gateway_options) do
-      {
-        order_id: "R423936067-5D5ZHURX",
-        email: "spree@example.com",
-        customer_id: 1,
-        currency: "USD",
-        ip: "1.2.3.4"
-      }
-    end
-    let(:card) { stub_model(Spree::CreditCard, gateway_customer_profile_id: "CARDIDATADYEN") }
-
-    before do
-      expect(adyen_response).to receive(:success?).at_least(:once) { true }
-      expect(adyen_response).to receive(:params).at_least(:once) { adyen_params }
-      expect(adyen_response).to receive(:psp_reference) { adyen_params[:psp_reference] }
-    end
-
-    context "when payment is successful" do
-      let(:adyen_params) do
-        {
-          psp_reference: "BEAUTIFULREFERENCE",
-          result_code: "Authorised",
-          auth_code: "ISSUER_AUTH",
-          additional_data: {},
-          refusal_reason: ""
-        }
-      end
-
-      it "calls the Adyen service with the right options and returns the correct object" do
-        expect(gateway.provider).to receive(:authorise_recurring_payment).with(
-          "R423936067",
-          { value: 2000, currency: "USD" },
-          { reference: 1, email: "spree@example.com", ip: "1.2.3.4", statement: "R423936067-5D5ZHURX" },
-          "CARDIDATADYEN",
-          nil,
-          true
-        ).and_return(adyen_response)
-
-        response = gateway.purchase(2000, card, gateway_options)
-        expect(response.success?).to be true
-        expect(response.message).to eq(
-          JSON.pretty_generate({
-            "psp_reference" => "BEAUTIFULREFERENCE",
-            "result_code" => "Authorised",
-            "auth_code" => "ISSUER_AUTH",
-            "additional_data" => {},
-            "refusal_reason" => ""
-          })
-        )
-        expect(response.params).to eq(
-          {
-            "psp_reference" => "BEAUTIFULREFERENCE",
-            "result_code" => "Authorised",
-            "auth_code" => "ISSUER_AUTH",
-            "additional_data" => {},
-            "refusal_reason" => ""
-          }
-        )
-        expect(response.authorization).to eq("BEAUTIFULREFERENCE")
-        expect(response.error_code).to be nil
-      end
+    it "tells the user it's a dummy response" do
+      expect(subject.message).to eq("dummy authorization response")
     end
   end
 
